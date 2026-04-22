4fd81001f52e63fe8e5c691bf744f8043b68c2ab
tundish <tundish@gigeconomy.org.uk>
2026-04-22 17:32:04 +0100

    Add Back and Next nav.

--- reciprocus/util/fixup.py
+++ reciprocus/util/fixup.py
@@ -311,7 +311,13 @@ def main(args):
                 pages[name] = (path, html5)
                 logger.info(f"Page created ({len(html5)} chars)", extra=dict(path=path))
 
-    for name, (path, html5) in pages.items():
+    for n, (name, (path, html5)) in enumerate(pages.items()):
+        if n > 0:
+            link = ""
+            html5 = html5.replace("</dl></nav>", f'<dt>Back</dt><dd class="back"><a href="{list(pages)[n - 1]}">{list(pages)[n - 1]}</a></dd></dl></nav>')
+        if n < len(pages) - 1:
+            link = ""
+            html5 = html5.replace("</dl></nav>", f'<dt>Next</dt><dd class="next"><a href="{list(pages)[n + 1]}">{list(pages)[n + 1]}</a></dd></dl></nav>')
         output = args.output / name
         output.write_text(html5)
         logger.info(f"Written to {output}", extra=dict(path=path))
